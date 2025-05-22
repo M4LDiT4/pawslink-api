a9bfe69d4df5f072d2e4d0e913bf2db259147d14
M4LDIT4 <calugas.jonathan.18@gmail.com>
2025-05-22 15:35:44 +0800

    added packages
    -added helment for http headers
    -added cors
    -added compression for compressed responses
    -added morgan for performance

--- server/core/express/index.js
+++ server/core/express/index.js
@@ -14,11 +14,21 @@
  */
 
 const express = require('express');
+const helmet = require('helmet');
+const compression = require("compression");
+const cors = require("cors");
+const morgan = require("morgan");
+
 
 module.exports = () => {
    let app = express();
 
    app.use(express.json());
+   app.use(helmet());
+   app.use(compression());
+   app.use(cors());
+   app.use(morgan('dev'));
+
 
    require('./initRoutes')(app);
    require('./generateApiResponse')(app);
